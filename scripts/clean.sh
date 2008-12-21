c1680423a4ec8f2d4ab8ac066a87e9734ad7fb8a
Saritha Kalyanam <kalyanamsaritha@gmail.com>
2008-12-20 21:22:34 -0800

    sym

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -10,3 +10,9 @@ find . | grep log$;
 echo "^C to quit";
 read ans;
 find . | grep log$ | xargs rm 2>/dev/null;
+
+
+find . | grep -- -$;
+echo "^C to quit";
+read ans;
+find . | grep -- -$ | xargs rm 2>/dev/null;
