c6621cbf56ea4d8ce2d6bc2e2d4792ff91077649
Saritha Kalyanam <kalyanamsaritha@gmail.com>
2008-12-25 11:31:54 -0800

    footprints

--- scripts/pkg/newlib/DB9-smt.pl
+++ scripts/pkg/newlib/DB9-smt.pl
@@ -10,6 +10,18 @@ use warnings;
 
 use Pcb_9;
 
+my %pinmap = ();
+$pinmap{1} = 1;
+$pinmap{2} = 6;
+$pinmap{3} = 2;
+$pinmap{4} = 7;
+$pinmap{5} = 3;
+$pinmap{6} = 8;
+$pinmap{7} = 4;
+$pinmap{8} = 9;
+$pinmap{9} = 5;
+
+
 my $pcb = Pcb_9->new(debug => 1);
 $pcb->element_begin(description => "DB9-smt",
 			output_file => "DB9-smt.fp",
@@ -29,17 +41,22 @@ for (my $i = 1; $i <= 9; $i++) {
 	$pcb->element_add_pad_rectangle(x => 272 + 55*($i-1), y => 0,
 			length => 32, width => 146,
 			mask => 10, clearance => 10,
-			name => $i, pin_number => $i);
+			name => "$pinmap{$i}", pin_number => "$pinmap{$i}");
 }
 
 $pcb->element_add_pin(x => 272 + 55*8 + 93, y => 0,
 			thickness => 59, drill_hole => 59,
 			mask => 10, clearance => 10,
-			name => 100, pin_number => 100);
+			name => 101, pin_number => 101);
 $pcb->element_add_pin(x => 272 + 55*8 + 93 + 179, y => 0,
 			thickness => 140, drill_hole => 120,
 			mask => 10, clearance => 10,
-			name => 10, pin_number => 10);
+			name => 11, pin_number => 11);
+
+$pcb->element_add_rectangle(x => 492, y => 0,
+			length => 1224, width => 236,
+			mask => 10, clearance => 10,
+			thickness => 5);
 
 $pcb->element_output();
 
